ea48fe34fbba7f4cbdb290f7da8b3f1917907cbe
Miodrag Mitic <miodrag.mitic@gmail.com>
2011-07-19 13:46:47 +0200

    preimenovana default Rils strana

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 SampleApp::Application.routes.draw do
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  root :to => "pages#home"
+#  match '/', :to => 'pages#home'
   get "pages/home"
 
   get "pages/contact"
@@ -6,6 +12,9 @@ SampleApp::Application.routes.draw do
   get "pages/about"
 
   get "pages/help"
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
